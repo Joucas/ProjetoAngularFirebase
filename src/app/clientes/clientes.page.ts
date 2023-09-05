58180b518a011e34a13244f05fb0889f06c60bcf
Joucas <joaquim.ipero@gmail.com>
2023-09-05 18:34:48 -0300

    Fazendo Listar e Remover

---
+++ src/app/clientes/clientes.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.page.html',
+  styleUrls: ['./clientes.page.scss'],
+})
+export class ClientesPage{
+
+  isLoading: boolean = false
+  clientes: any = []
+
+  getClientes(){
+    this.isLoading = true
+    fetch('http://localhost/api/clientes/listar.php')
+    .then(res => res.json())
+    .then(dados => { this.clientes = dados['clientes'] })
+    .catch(error => { console.log(error)} )
+    .finally(() =>{ 
+      this.isLoading = false
+      console.log('Processo finalizado!') 
+    })
+  }
+
+  removerCliente(CodCli: any){
+    this.isLoading = true
+    fetch('http://localhost/api/clientes/remover.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ CodCli: CodCli }),
+
+    })
+    .then(res => res.json())
+    .then(dados => { console.log(dados)})
+    .catch(error => { console.log(error)} )
+    .finally(() =>{ 
+      this.isLoading = false
+      console.log('Processo finalizado!') 
+    })
+  }
+
+}
